379b1fc20abe1aaaaaaf93c3273dca2717660cb1
Christian Acosta <christianjavieracosta@outlook.com>
2026-09-08 10:37:36 -0400

    Create basic structure for linked list queue
    Initial implementation of a C++ program for a linked list queue simulation.

---
+++ programa.cpp
@@ -0,0 +1,25 @@
+/*
+12/9/26
+Grupo 1:
+- Alessandro Salamone - 1132116
+- Carlos Minaya - 1132836
+- Axel Almonte - 1131078
+- ⁠Octavio Ramírez - 1132995
+- ⁠José Pinales - 1133255
+- Christian Acosta - 1132698
+
+Realizar un programa C++ que simule una Lista Enlazada Cola (Linked List).
+*/
+
+#include <iostream>
+using namespace std;
+
+struct LinkedList {
+int dato;
+LinkedList *next;
+};
+
+int main()
+{
+  return 0;
+}
